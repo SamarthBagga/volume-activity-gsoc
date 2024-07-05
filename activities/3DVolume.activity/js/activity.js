deb0d831d5c44aa0b454120f575103bab24006d1
SamarthBagga <samarthbagga@gmail.com>
2024-07-05 20:42:42 +0530

    changes

--- activities/3DVolume.activity/js/activity.js
+++ activities/3DVolume.activity/js/activity.js
@@ -1083,7 +1083,7 @@ define([
 		var readyToWatch = false;
 		console.log(useragent.indexOf('android'))
 		if (useragent.indexOf('android') != -1) {
-			alert("IT IS WORKING")
+			window.alert("IT IS WORKING")
 		}
 
 		// if (useragent.indexOf('android') != -1 || useragent.indexOf('iphone') != -1 || useragent.indexOf('ipad') != -1 || useragent.indexOf('ipod') != -1 || useragent.indexOf('mozilla/5.0 (mobile') != -1) {
